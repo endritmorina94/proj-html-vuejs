4631fba7ecec238223e9834e00d08010a5059f71
endritmorina94 <endrit.morina@outlook.it>
2021-05-10 13:44:17 +0200

    finite navbar

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,23 @@
   \********************/
 /***/ (() => {
 
-
+var app = new Vue({
+  el: '#root',
+  data: {
+    contactInfo: {
+      daysAvailability: "Mon - Sat",
+      timeAvailability: "9:00 - 18:00",
+      phone: "+1 (305) 1234-5678",
+      email: "hello@example.com",
+      facebook_link: "#",
+      twitter_link: "#",
+      linkedin_link: "#"
+    },
+    sections: ["Home", "Services", "About", "Projects", "Results"]
+  },
+  methods: {},
+  mounted: function mounted() {}
+});
 
 /***/ }),
 
