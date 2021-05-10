307f9fcb6df84826cc2b398fe25e55d0f9d6177b
endritmorina94 <endrit.morina@outlook.it>
2021-05-11 00:19:15 +0200

    finito services section

--- dist/app.js
+++ dist/app.js
@@ -19,7 +19,32 @@ var app = new Vue({
       twitter_link: "#",
       linkedin_link: "#"
     },
-    sections: ["Home", "Services", "About", "Projects", "Results"]
+    sections: ["Home", "Services", "About", "Projects", "Results"],
+    services: [{
+      name: "Audit & Assurance",
+      description: "Lorem ipsum dolor si amet consectetur adipiscing elit.",
+      icon: "fas fa-sitemap"
+    }, {
+      name: "Financial Advisory",
+      description: "Lorem ipsum dolor si amet consectetur adipiscing elit.",
+      icon: "fas fa-briefcase"
+    }, {
+      name: "Analytics and M&A",
+      description: "Lorem ipsum dolor si amet consectetur adipiscing elit.",
+      icon: "fas fa-chart-bar"
+    }, {
+      name: "Middle Marketing",
+      description: "Lorem ipsum dolor si amet consectetur adipiscing elit.",
+      icon: "fas fa-plane"
+    }, {
+      name: "Legal Consulting",
+      description: "Lorem ipsum dolor si amet consectetur adipiscing elit.",
+      icon: "fas fa-globe"
+    }, {
+      name: "Regulatory Risk",
+      description: "Lorem ipsum dolor si amet consectetur adipiscing elit.",
+      icon: "fas fa-inbox"
+    }]
   },
   methods: {},
   mounted: function mounted() {}
--- src/app.js
+++ src/app.js
@@ -17,6 +17,38 @@ var app = new Vue(
                 "About",
                 "Projects",
                 "Results"
+            ],
+            services: [
+                {
+                    name: "Audit & Assurance",
+                    description: "Lorem ipsum dolor si amet consectetur adipiscing elit.",
+                    icon: "fas fa-sitemap"
+                },
+                {
+                    name: "Financial Advisory",
+                    description: "Lorem ipsum dolor si amet consectetur adipiscing elit.",
+                    icon: "fas fa-briefcase"
+                },
+                {
+                    name: "Analytics and M&A",
+                    description: "Lorem ipsum dolor si amet consectetur adipiscing elit.",
+                    icon: "fas fa-chart-bar"
+                },
+                {
+                    name: "Middle Marketing",
+                    description: "Lorem ipsum dolor si amet consectetur adipiscing elit.",
+                    icon: "fas fa-plane"
+                },
+                {
+                    name: "Legal Consulting",
+                    description: "Lorem ipsum dolor si amet consectetur adipiscing elit.",
+                    icon: "fas fa-globe"
+                },
+                {
+                    name: "Regulatory Risk",
+                    description: "Lorem ipsum dolor si amet consectetur adipiscing elit.",
+                    icon: "fas fa-inbox"
+                }
             ]
         },
         methods: {
